0696f909e13c428949006e6e1555732ff34a8860
BrianNkwera <briansimon487@yahoo.com>
2025-07-05 03:01:00 +0300

    change vite base to resolve cors errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://github.com/BrianNkwera/CRDB-Landing-Page/'
+  base: '/CRDB-Landing-Page/'
 });
